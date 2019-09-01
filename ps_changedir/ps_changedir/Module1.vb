02d95f58298f633fd9cbcdf5aa99e44a7fdbc339
wirokocaveman <wiroko.caveman@gmail.com>
2019-09-02 00:48:10 +0200

    new project

---
+++ ps_changedir/ps_changedir/Module1.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim InputFile() As String = {"d:\temp\test", "d:temp\test1"}
+        Dim Counter As Integer = 0
+        Dim InputCounter As Integer
+        For Each dirtext As String In InputFile
+            Console.WriteLine("{0} {1}", Counter, dirtext)
+            Counter += 1
+        Next
+        Console.Write("CommandLine number") : InputCounter = Console.ReadLine()
+
+        Console.WriteLine(InputFile(InputCounter))
+        Console.ReadLine()
+    End Sub
+
+
+End Module
